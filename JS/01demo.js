0a6dc4218cbfa9cfe437dcd4d7faee37878e0097
lidi1987227 <289641796@qq.com>
2017-06-29 21:23:23 +0800

    20170629worksapcs-up

---
+++ JS/01demo.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/6/22 0022.
+ */
+    console.log("" && "ср╠ъ");
+    console.log(null && "ср╠ъ");
+    console.log(undefined && "ср╠ъ");
+    console.log("вС╠ъ" || "ср╠ъ");
+    console.log(null && "ср╠ъ");
